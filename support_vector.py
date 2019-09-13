5ce476f763eb329357bd554ca5fdf5385a9d2981
jang won park <jangwonpark74@gmail.com>
2019-09-13 15:32:54 +0900

    Update support_vector.py

--- support_vector.py
+++ support_vector.py
@@ -3,7 +3,7 @@ import math
 import numpy as np 
 import matplotlib.pyplot as plt
 
-from sklearn import linear_model
+from sklearn import svm
 
 x = np.random.rand(1000,1)
 x = x *20 -10
@@ -11,7 +11,7 @@ x = x *20 -10
 y = np.array([math.sin(v) for v in x])
 y += np.random.randn(1000)
 
-model = linear_model.LinearRegression()
+model = svm.SVR()
 model.fit( x, y)
 
 print(model.score(x,y))

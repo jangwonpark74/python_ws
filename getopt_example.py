733acf9a56a7539dae53c47f39f7de04c256ac76
jang won park <jangwonpark74@gmail.com>
2019-09-21 12:18:31 +0900

    Create getopt_example.py

---
+++ getopt_example.py
@@ -0,0 +1,26 @@
+import getopt
+import sys
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "ho:v", ["help", "output="])
+    except getopt.GetoptError as err:
+        print(err)
+        usage()
+        sys.exit(2)
+    output = None
+    verbose = False
+
+    for o, a in opts:
+        if o == "-v"
+            verbose = True
+        elif o in ("-h", "--help") :
+            usage()
+            sys.exit()
+        elif o in ("-o", "--output"):
+            output = a
+        else:
+            assert False, "unhandled option"
+
+if __name__ == "__main__":
+    main()

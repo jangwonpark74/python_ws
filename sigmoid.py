142491edb1a61d9c2099954c9277d28abe2ebc66
jang won park <jangwonpark74@gmail.com>
2019-09-21 20:17:59 +0900

    Update sigmoid.py

--- sigmoid.py
+++ sigmoid.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np 
 
 def basic_sigmoid(x):
     s = 1 / (1 + math.exp(-x))
@@ -6,3 +7,11 @@ def basic_sigmoid(x):
 
 val = basic_sigmoid(3)
 print("value=" + str(val))
+
+x = [1, 2, 3, 4]
+print("x= " + str(x))
+y = np.array(x)
+print("y= " +str(y))
+
+val = 1/(1+ np.exp(x))
+print("val=" + str(val))

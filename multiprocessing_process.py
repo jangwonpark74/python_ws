0da610673a6bd2195775221de471bd1dc8117abc
jang won park <jangwonpark74@gmail.com>
2019-09-22 06:19:11 +0900

    Create multiprocessing_process.py

---
+++ multiprocessing_process.py
@@ -0,0 +1,15 @@
+import random
+from multiprocessing import Manager, Process
+
+def compute(results):
+    results.append(sum([random.randint(1, 100) for i in range(1000000)]))
+
+if __name__ == '__main__':
+    with Manager() as manager:
+        results = manager.list()
+        workers = [Process(target=compute, args=(results,)) for i in range(8)]
+        for worker in workers:
+            worker.start()
+        for worker in workers:
+            worker.join()
+        print("Results : %s" % results
